7e94123f7d91362bc88164a8f65541f5decbfef8
Er. Himanshu Raj <96337118+raj04h@users.noreply.github.com>
2024-08-22 22:33:15 +0530

    Update APIFile.kt
    Your API Key

--- app/src/main/java/com/hr/hub/ui/APIFile.kt
+++ app/src/main/java/com/hr/hub/ui/APIFile.kt
@@ -2,7 +2,7 @@ package com.hr.hub.ui
 
 object APIFile {
 
-    val geminiAPI="AIzaSyDwfhNfKUzIpyuaCyKjuGBEbauBwPt4ngU"
+    val geminiAPI="Use Your Key"
 
-    val newsAPI="69d4de55a8ad42e3a255b7fc0bd703e5"
+    val newsAPI="Your Key"
 }
